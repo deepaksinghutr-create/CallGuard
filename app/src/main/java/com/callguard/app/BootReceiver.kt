26045a8fbb09630e85b7ac3f1ee4ef543c0c16fd
deepaksinghutr-create <deepaksinghutr@gmail.com>
2026-08-02 21:02:09 +0530

    Create BootReceiver.kt

---
+++ app/src/main/java/com/callguard/app/BootReceiver.kt
@@ -0,0 +1,17 @@
+package com.callguard.app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        val serviceIntent = Intent(context, RingDetectorService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent)
+        } else {
+            context.startService(serviceIntent)
+        }
+    }
+}
